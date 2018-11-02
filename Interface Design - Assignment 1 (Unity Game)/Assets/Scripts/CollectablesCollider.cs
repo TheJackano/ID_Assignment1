60bcc045a73e121ed8c255675c1828589a38e27e
Jesse Nunes <s5081040@bournemouth.ac.uk>
2018-11-02 09:53:25 +0000

    Controller, collider and data scripts for the collectables. Also complete the save and load functionality.

---
+++ Interface Design - Assignment 1 (Unity Game)/Assets/Scripts/CollectablesCollider.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectablesCollider : MonoBehaviour {
+
+	CollectablesController cc;
+
+	void Start()
+	{
+		GameObject ccgo = GameObject.Find ("CollectablesController");
+		cc = ccgo.GetComponent<CollectablesController>();
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		if (gameObject.name.Contains("Aid Box"))
+		{
+			Debug.Log ("You've collected a useful aid box!");
+		}
+		else if (gameObject.name.Contains("Gem"))
+		{
+			Debug.Log ("You've collected a shiny gem!");
+		}
+		else if (gameObject.name.Contains("Chicken Leg"))
+		{
+			Debug.Log ("You've collected a tasty chicken leg!");
+		}
+		//AudioSource audio = GetComponent<AudioSource>();
+		//audio.Play();
+
+		cc.IncrementCount (gameObject);
+		Destroy (gameObject);
+	}
+}
